59287a174eb9c3ff05164c53df2d3226a529e2ab
Roman Vitenbergskiy <romanforresume@gmail.com>
2025-02-20 14:10:56 +0200

    findByCustomerId added to CustomerProperties repository.

--- backend/src/main/java/com/romanwit/minicrm/repository/CustomerPropertyRepository.java
+++ backend/src/main/java/com/romanwit/minicrm/repository/CustomerPropertyRepository.java
@@ -1,7 +1,12 @@
 package com.romanwit.minicrm.repository;
 
 import com.romanwit.minicrm.model.CustomerProperty;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerPropertyRepository extends JpaRepository<CustomerProperty, Long> {}
+public interface CustomerPropertyRepository extends JpaRepository<CustomerProperty, Long> {
+	List<CustomerProperty> findByCustomerId(Long customerId);
+}
 
