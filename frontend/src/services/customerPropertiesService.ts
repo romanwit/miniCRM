2b8de53eac5ea13387430764b99f86073e8277e5
Roman Vitenbergskiy <romanforresume@gmail.com>
2025-02-20 14:14:15 +0200

    Customer properties service added to frontend

---
+++ frontend/src/services/customerPropertiesService.ts
@@ -0,0 +1,28 @@
+import { getToken } from './authService';
+import { baseUrl } from './constService';
+
+export const getAllProperties = async ():Promise<Property[]> => {
+
+    var result: Property[] = [];
+
+    const token = getToken();
+
+    try {
+        const response = await fetch(baseUrl + "/api/customer-properties", {
+          method: "GET",
+          headers: { 
+            "Authorization": `Bearer ${token}`
+          }
+        });
+    
+        if (!response.ok) throw new Error("Failed to read customer properties");
+        result = await response.json();
+        
+      } catch (error) {
+        console.error('Error reading customer properties:', error);
+        alert("Error reading customer properties. Please try again.");
+      }
+
+    return result;
+
+}
