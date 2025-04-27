dc94b2e25077160f8ef939f427e2b147b00102e2
Roman Vitenbergskiy <romanforresume@gmail.com>
2025-04-27 10:56:41 +0300

    Cleanup.

--- backend/src/main/java/com/romanwit/minicrm/repository/CustomerRepository.java
+++ backend/src/main/java/com/romanwit/minicrm/repository/CustomerRepository.java
@@ -8,6 +8,4 @@ import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     boolean existsByName(String name);
-    // @Query("SELECT c FROM Client c LEFT JOIN FETCH c.properties")
-    // List<Client> findAllWithProperties();
 }
